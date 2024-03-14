a1e676e4a1d47cf616cc52a812be73a38062af4b
Gonzalez <gonzalezlouis1981@gmail.com>
2024-03-14 17:02:24 +0100

    premier pas symfony

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,31 @@
+<?php
+
+// On déclare le namespace de la classe
+namespace App\Controller;
+
+// On inclut les classes dont on a besoin
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+// On declare la classe HomepageController
+class HomepageController extends AbstractController
+{
+    // On declare la fonction index
+    // le # sert a dire qu'il n'y a pas de params : c'est l'attribut name de la route
+    #[Route('/', name: 'app_home',methods:['GET'])]
+    public function index(): Response
+    {
+        
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/home', name: 'app_homepage_alt',methods:['GET'])]
+    #[Route('/homepage', name: 'app_homepage',methods:['GET'])]
+    public function homepage(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_home');
+    }
+}
